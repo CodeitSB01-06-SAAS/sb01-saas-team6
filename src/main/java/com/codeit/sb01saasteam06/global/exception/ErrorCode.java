789588f9fa398b584ecdf2a746da3be84406eb25
hyojkim2 <skql2548@gmail.com>
2025-06-24 13:37:54 +0900

    :sparkles: schema 업로드

--- src/main/java/com/codeit/sb01saasteam06/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/sb01saasteam06/global/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 	//global
 	INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
 	INVALID_REQUEST("잘못된 요청입니다"),
-	ILLEGAL_ARGUMENT_ERROR("잘못된 인수가 전달되었습니다.")
+	ILLEGAL_ARGUMENT_ERROR("잘못된 인수가 전달되었습니다!.")
 
 
 
