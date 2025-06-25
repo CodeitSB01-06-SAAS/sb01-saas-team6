c849906ba6fad5b975a47725edab9d804e578f47
GiMin0123 <minby260306@gmail.com>
2025-06-25 16:19:33 +0900

    :recycle: querydsl관련 q클래스 에러 주석처리 #20

--- src/main/java/com/codeit/sb01saasteam06/config/QueryDslConfig.java
+++ src/main/java/com/codeit/sb01saasteam06/config/QueryDslConfig.java
@@ -1,5 +1,6 @@
 package com.codeit.sb01saasteam06.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
