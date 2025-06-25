9d1067fe9d7a20b62f0918fad6ab6812a30833f8
kbh <xjvm7001@naver.com>
2025-06-25 18:43:20 +0900

    :recycle:  피드 좋아요 엔티티 어노테이션과 생성자 누락 리펙토링 #22

--- src/main/java/com/codeit/sb01saasteam06/domain/feed/entity/FeedLike.java
+++ src/main/java/com/codeit/sb01saasteam06/domain/feed/entity/FeedLike.java
@@ -7,6 +7,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "feeds_likes",
@@ -17,6 +20,8 @@ import jakarta.persistence.UniqueConstraint;
         )
     } // 복합키 써서, 피드에 사용자가 한번만 좋아요 누룰 수 있게 설정
 )
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FeedLike extends BaseUpdatableEntity {
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -27,4 +32,8 @@ public class FeedLike extends BaseUpdatableEntity {
   @JoinColumn(name = "user_id", nullable = false)
   private User user; //연관된 피드 아이디
 
+  public FeedLike(Feed feed, User user) {
+    this.feed = feed;
+    this.user = user;
+  }
 }
