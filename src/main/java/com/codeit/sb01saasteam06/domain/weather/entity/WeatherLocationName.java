551815e9a686f49023908b0a311179bff57edc36
GiMin0123 <minby260306@gmail.com>
2025-06-25 16:12:16 +0900

    :sparkles: Weather 엔티티 추가  및 DDL 수정

---
+++ src/main/java/com/codeit/sb01saasteam06/domain/weather/entity/WeatherLocationName.java
@@ -0,0 +1,33 @@
+package com.codeit.sb01saasteam06.domain.weather.entity;
+
+import com.codeit.sb01saasteam06.domain.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "weather_location_names")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WeatherLocationName extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "weather_id")
+    private Weather weather;
+
+    private String locationName;
+
+    private WeatherLocationName(Weather weather, String locationName) {
+        this.weather = weather;
+        this.locationName = locationName;
+    }
+
+    public static WeatherLocationName of(Weather weather, String locationName) {
+        return new WeatherLocationName(weather, locationName);
+    }
+}
