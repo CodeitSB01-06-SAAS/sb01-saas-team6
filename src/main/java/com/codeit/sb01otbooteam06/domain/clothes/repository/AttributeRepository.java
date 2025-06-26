e3b1c6621d5d02481fb1ce2ef5835d4734b5559e
hyojkim2 <skql2548@gmail.com>
2025-06-26 18:12:33 +0900

    :truck: 의상 속성 네임 변경

--- src/main/java/com/codeit/sb01otbooteam06/domain/clothes/repository/AttributeRepository.java
+++ src/main/java/com/codeit/sb01otbooteam06/domain/clothes/repository/AttributeRepository.java
@@ -1,11 +1,11 @@
 package com.codeit.sb01otbooteam06.domain.clothes.repository;
 
-import com.codeit.sb01otbooteam06.domain.clothes.entity.AttributesDef;
+import com.codeit.sb01otbooteam06.domain.clothes.entity.Attribute;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttributesDefRepository extends JpaRepository<AttributesDef, UUID> {
+public interface AttributeRepository extends JpaRepository<Attribute, UUID> {
 
 }
