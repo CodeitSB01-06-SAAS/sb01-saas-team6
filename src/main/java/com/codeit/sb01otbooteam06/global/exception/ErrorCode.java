c2dbd7c11b0485ed5a8810023d53b6f2011c565f
hyojkim2 <skql2548@gmail.com>
2025-06-26 18:24:23 +0900

    :sparkles: 의상 NOT FOUND 에러코드 추가

--- src/main/java/com/codeit/sb01otbooteam06/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/sb01otbooteam06/global/exception/ErrorCode.java
@@ -9,8 +9,10 @@ public enum ErrorCode {
   //global
   INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
   INVALID_REQUEST("잘못된 요청입니다"),
-  ILLEGAL_ARGUMENT_ERROR("잘못된 인수가 전달되었습니다.");
+  ILLEGAL_ARGUMENT_ERROR("잘못된 인수가 전달되었습니다."),
 
+  //Clothes
+  CLOTHES_NOT_FOUND("의상이 없습니다.");
 
   private final String message;
 
