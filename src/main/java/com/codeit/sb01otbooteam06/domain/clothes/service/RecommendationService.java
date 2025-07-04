92b3555e5ed09ebde2c454c7bd3b79dfb1c0a03d
hyojkim2 <skql2548@gmail.com>
2025-07-04 18:00:20 +0900

    :sparkles: 추천 기능 - 가짜 Ootd데이터 반환

--- src/main/java/com/codeit/sb01otbooteam06/domain/clothes/service/RecommendationService.java
+++ src/main/java/com/codeit/sb01otbooteam06/domain/clothes/service/RecommendationService.java
@@ -1,8 +1,16 @@
 package com.codeit.sb01otbooteam06.domain.clothes.service;
 
 
+import com.codeit.sb01otbooteam06.domain.clothes.entity.Clothes;
+import com.codeit.sb01otbooteam06.domain.clothes.entity.dto.OotdDto;
 import com.codeit.sb01otbooteam06.domain.clothes.entity.dto.RecommendationDto;
+import com.codeit.sb01otbooteam06.domain.clothes.mapper.ClothesMapper;
+import com.codeit.sb01otbooteam06.domain.user.entity.User;
+import com.codeit.sb01otbooteam06.domain.user.repository.UserRepository;
 import com.codeit.sb01otbooteam06.domain.weather.repository.WeatherRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +23,10 @@ public class RecommendationService {
   //todo: 서비스? 웨더 확인해보고 추후 변경
   private final WeatherRepository weatherRepository;
 
+  private final UserRepository userRepository;
+
+  private final ClothesMapper clothesMapper;
+
   //TODO: 의상 추천 알고리즘
   //날씨 데이터, 사용자가 등록한 의상, 프로필 정보를 활용하여 의상을 추천
   @Transactional
@@ -23,9 +35,28 @@ public class RecommendationService {
     //todo: 유저 아이디 획득
     UUID userId = UUID.randomUUID();
 
+    //날씨
+
+    // 온도
+
     //todo: 시간이 소요될 것으로 예상되어 우선 임시 데이터 던지게하기
-    return null;
+    List<OotdDto> result = new ArrayList<>();
+    result.add(makeDummyOotdDto("상의", "TOP"));
+    result.add(makeDummyOotdDto("하의", "BOTTOM"));
+    result.add(makeDummyOotdDto("아우터", "OUTER"));
+    result.add(makeDummyOotdDto("모자", "HAT"));
+    return new RecommendationDto(weatherId, userId, result);
   }
 
+  //admin의 더미 Ootd데이터 생성
+  private OotdDto makeDummyOotdDto(String name, String type) {
+
+    User user = userRepository.findByEmail("admin@example.com")
+        .orElseThrow(() -> new NoSuchElementException());
+    Clothes clothes = new Clothes(user, name, type, null);
+
+    return OotdDto.toDto(clothesMapper.toDto(clothes));
+
+  }
 
 }
