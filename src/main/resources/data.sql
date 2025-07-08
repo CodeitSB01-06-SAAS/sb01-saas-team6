c0a19e031b12a843249d1f9a40fe8f4619486e6f
hyojkim2 <skql2548@gmail.com>
2025-07-08 18:48:57 +0900

    :recycle: data.sql 컬럼명추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,12 +8,12 @@ VALUES
 
 
 --User
-INSERT INTO users
+INSERT INTO users(id, email, password, name, role, locked, created_at, linked_oauth_providers)
 --테스트유저의 실 비밀번호는 test1234!
 VALUES
     ('6e54fc4a-f8f0-478d-a54e-ebe573316061', 'test@test.com', '$2a$10$28/I0KoP1GKVXGg22B6PQeMRYqKvMUqJ7E7XWhIIDHHISpJhjnoIG', '테스트유저', 'USER', false, '2025-07-08 09:21:33.619871', null );
 
 --Profiles
-INSERT INTO profiles
+INSERT INTO profiles (id, name, gender, birth_date, latitude, longitude , x, y, location_names, temperature_sensitivity, profile_image_url, created_at, updated_at)
 VALUES
     ('6e54fc4a-f8f0-478d-a54e-ebe573316061', '테스트유저', 'OTHER', '2025-07-08', 0, 0, 0, 0, null, 3, null, '2025-07-08 09:21:33.624865', '2025-07-08 09:21:33.624865');
