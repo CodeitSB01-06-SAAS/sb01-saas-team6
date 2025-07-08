a9a3e1315905d38b6c9f43351e55a496c7a5e4e9
kbh <xjvm7001@naver.com>
2025-07-08 16:19:27 +0900

    :pencil2: 주석 추가#51

--- src/main/java/com/codeit/sb01otbooteam06/domain/feed/service/impl/FeedLikeServiceImpl.java
+++ src/main/java/com/codeit/sb01otbooteam06/domain/feed/service/impl/FeedLikeServiceImpl.java
@@ -32,40 +32,41 @@ public class FeedLikeServiceImpl implements FeedLikeService {
     User user = userRepository.findById(userId)
         .orElseThrow(() -> new OtbooException(ErrorCode.USER_NOT_FOUND));
 
-
     Feed feed = feedRepository.findById(feedId)
         .orElseThrow(() -> new OtbooException(ErrorCode.FEED_NOT_FOUND));
 
     // 좋아요 중복 로직
-    if(feedLikeRepository.existsByFeedAndUser(feed, user)){
+    if (feedLikeRepository.existsByFeedAndUser(feed, user)) {
       throw new OtbooException(ErrorCode.ILLEGAL_ARGUMENT_ERROR);
-     }
+    }
 
     // 좋아요 생성 & DB 저장
-     FeedLike like = new FeedLike(feed, user);
-     feedLikeRepository.save(like);
+    FeedLike like = new FeedLike(feed, user);
+    feedLikeRepository.save(like);
 
     //피드 카운드 증가 -> dirty checking 으로 자동 반영
     feed.like();
+
+    //todo : 나중에 좋아요 누를때 알림 가도록 설정
 
   }
 
   @Transactional
   @Override
   public void unlikeFeed(UUID feedId) {
-     UUID userId = authService.getCurrentUserId();
-     User user = userRepository.findById(userId)
+    UUID userId = authService.getCurrentUserId();
+    User user = userRepository.findById(userId)
         .orElseThrow(() -> new OtbooException(ErrorCode.USER_NOT_FOUND));
 
     Feed feed = feedRepository.findById(feedId)
         .orElseThrow(() -> new OtbooException(ErrorCode.FEED_NOT_FOUND));
 
     // 기존 좋아요 엔티티 조회
-      FeedLike like = feedLikeRepository.findByFeedAndUser(feed, user)
+    FeedLike like = feedLikeRepository.findByFeedAndUser(feed, user)
         .orElseThrow(() -> new OtbooException(ErrorCode.ILLEGAL_ARGUMENT_ERROR));
 
     // 좋아요 취소
-     feedLikeRepository.delete(like);
+    feedLikeRepository.delete(like);
 
     // 피드 좋아요 수 감소
     feed.unlike();
