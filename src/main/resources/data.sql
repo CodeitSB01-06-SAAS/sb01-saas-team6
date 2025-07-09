ee4112ed0d4f9b92826d059c952515b2f085b247
hyojkim2 <skql2548@gmail.com>
2025-07-09 17:24:34 +0900

    :sparkles: data.sql 쿼리 순서 변경, 테스트 유저의 의상 & 의상-속성 중간테이블 쿼리 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,16 @@
+--User
+INSERT INTO users(id, email, password, name, role, locked, created_at, linked_oauth_providers)
+--테스트유저의 실 비밀번호는 test1234!
+VALUES
+    ('6e54fc4a-f8f0-478d-a54e-ebe573316061', 'test@test.com', '$2a$10$28/I0KoP1GKVXGg22B6PQeMRYqKvMUqJ7E7XWhIIDHHISpJhjnoIG', '테스트유저', 'USER', false, '2025-07-08 09:21:33.619871', null );
+
+--Profiles
+INSERT INTO profiles (id, name, gender, birth_date, latitude, longitude , x, y, location_names, temperature_sensitivity, profile_image_url, created_at, updated_at)
+VALUES
+    ('6e54fc4a-f8f0-478d-a54e-ebe573316061', '테스트유저', 'OTHER', '2025-07-08', 0, 0, 0, 0, null, 3, null, '2025-07-08 09:21:33.624865', '2025-07-08 09:21:33.624865');
+
+
+
 --의상 속성
 INSERT INTO attributes_defs (id, name, selectable_values, created_at, updated_at)
 VALUES
@@ -10,16 +23,22 @@ VALUES
     ('75723d92-6654-4976-8034-0b34a0a2ccf7', '스타일', ARRAY['캐주얼', '스트릿', '스포티', '미니멀', '시크', '레트로' ], '2025-07-08T00:34:58.223283Z', null),
     ('85723d92-6654-4976-8034-0b34a0a2ccf7', '신축성', ARRAY['없음', '약간 있음', '있음' ], '2025-07-08T00:34:58.223283Z', null);
 
-
-
-
---User
-INSERT INTO users(id, email, password, name, role, locked, created_at, linked_oauth_providers)
---테스트유저의 실 비밀번호는 test1234!
+-- 의상
+INSERT INTO clothes (id, owner_id, name, type, image_url, created_at, updated_at)
 VALUES
-    ('6e54fc4a-f8f0-478d-a54e-ebe573316061', 'test@test.com', '$2a$10$28/I0KoP1GKVXGg22B6PQeMRYqKvMUqJ7E7XWhIIDHHISpJhjnoIG', '테스트유저', 'USER', false, '2025-07-08 09:21:33.619871', null );
+    --테스트 유저 의상
+    ('e68bd02c-347b-4c9b-8243-353e8e7ae959', '6e54fc4a-f8f0-478d-a54e-ebe573316061', '상의', 'TOP', null,  '2025-07-09 07:43:36.588879', '2025-07-09 07:43:36.588879'
+       );
 
---Profiles
-INSERT INTO profiles (id, name, gender, birth_date, latitude, longitude , x, y, location_names, temperature_sensitivity, profile_image_url, created_at, updated_at)
+
+-- 의상 속성 중간테이블
+INSERT INTO clothes_attributes (id, clothes_id, attribute_id, value, created_at, updated_at)
 VALUES
-    ('6e54fc4a-f8f0-478d-a54e-ebe573316061', '테스트유저', 'OTHER', '2025-07-08', 0, 0, 0, 0, null, 3, null, '2025-07-08 09:21:33.624865', '2025-07-08 09:21:33.624865');
+    --테스트 유저의 '상의' 의상 속성 중간 정보
+    ('855c08b1-af6d-488e-aff0-d0a624fa0b66', 'e68bd02c-347b-4c9b-8243-353e8e7ae959', '45723d92-6654-4976-8034-0b34a0a2ccf7', '여름','2025-07-09 07:43:36.588879', '2025-07-09 07:43:36.588879'
+    ),
+    ('4e6c22a7-4bcb-4d63-b5cd-fe067fce2344', 'e68bd02c-347b-4c9b-8243-353e8e7ae959', '55723d92-6654-4976-8034-0b34a0a2ccf7', '얇음','2025-07-09 07:43:36.588879', '2025-07-09 07:43:36.588879'
+    );
+
+
+
