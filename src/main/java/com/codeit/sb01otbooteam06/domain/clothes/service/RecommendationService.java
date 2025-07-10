6ff61f5d5ca75154dcb2b1adc20032e4b720c6b4
hyojkim2 <skql2548@gmail.com>
2025-07-10 10:07:59 +0900

    :sparkles: 날씨 추천 서비스 - 날씨 데이터 추출 메서드 작성

--- src/main/java/com/codeit/sb01otbooteam06/domain/clothes/service/RecommendationService.java
+++ src/main/java/com/codeit/sb01otbooteam06/domain/clothes/service/RecommendationService.java
@@ -11,9 +11,14 @@ import com.codeit.sb01otbooteam06.domain.clothes.mapper.ClothesMapper;
 import com.codeit.sb01otbooteam06.domain.clothes.repository.ClothesAttributeRepository;
 import com.codeit.sb01otbooteam06.domain.clothes.repository.ClothesRepository;
 import com.codeit.sb01otbooteam06.domain.clothes.utils.ClothesUtils;
+import com.codeit.sb01otbooteam06.domain.profile.entity.Profile;
+import com.codeit.sb01otbooteam06.domain.profile.exception.ProfileNotFoundException;
+import com.codeit.sb01otbooteam06.domain.profile.repository.ProfileRepository;
 import com.codeit.sb01otbooteam06.domain.user.entity.User;
 import com.codeit.sb01otbooteam06.domain.user.exception.UserNotFoundException;
 import com.codeit.sb01otbooteam06.domain.user.repository.UserRepository;
+import com.codeit.sb01otbooteam06.domain.weather.entity.Weather;
+import com.codeit.sb01otbooteam06.domain.weather.exception.WeatherNotFoundException;
 import com.codeit.sb01otbooteam06.domain.weather.repository.WeatherRepository;
 import com.google.genai.Client;
 import java.util.ArrayList;
@@ -37,6 +42,7 @@ public class RecommendationService {
   //todo: 서비스? 웨더 확인해보고 추후 변경
   private final WeatherRepository weatherRepository;
   private final UserRepository userRepository;
+  private final ProfileRepository profileRepository;
 
   private final ClothesMapper clothesMapper;
   private final ClothesRepository clothesRepository;
@@ -49,60 +55,7 @@ public class RecommendationService {
   @Transactional
   public RecommendationDto create(UUID weatherId) {
 
-    //현재 로그인한 유저의 아이디를 획득한다.
-    UUID userId = authService.getCurrentUserId();
-
-    User user = userRepository.findById(userId).orElseThrow(
-        () -> new UserNotFoundException(userId)
-    );
-
-    /**날씨 데이터
-     * 1. 기온, 바람 , 습도, 날씨, 계절
-     *
-     * 의상 속성
-     * 1. 두께감, 계절,안감 ,따뜻한 정도
-     * */
-
-    //TODO날씨 데이터 생기면 열기
-//    //날씨 데이터 가져오기
-//    Weather weather = weatherRepository.findById(weatherId)
-//        .orElseThrow(() -> new WeatherNotFoundException());
-//
-//    //날씨 데이터에서 필요한 것 추출
-//    // humidity, temperature, windSpeed
-//    List<Long> weatherThings = new ArrayList<>();
-//    String skyStatus = String.valueOf(weather.getSkyStatus());
-
-    // 1. 의상 리스트 가져오기
-    List<Clothes> clothesList = clothesRepository.findAllByOwner(user);
-
-// 2. 의상 ID 리스트 추출
-    List<UUID> clothesIds = clothesList.stream()
-        .map(Clothes::getId)
-        .toList();
-
-// 3. 의상 속성들 한꺼번에 가져오기 (의상 리스트로)
-    List<ClothesAttribute> clothesAttributes = clothesAttributeRepository.findByClothesIn(
-        clothesList);
-
-// 4. 의상 ID별 속성 매핑 (Map<ClothesId, List<ClothesAttribute>>)
-    Map<UUID, List<ClothesAttribute>> attributesByClothesId = clothesAttributes.stream()
-        .collect(Collectors.groupingBy(attr -> attr.getClothes().getId()));
-
-// 5. ClothesDto 리스트 만들기
-    List<ClothesDto> clothesDtoList = clothesList.stream()
-        .map(clothes -> clothesUtils.makeClothesDto(
-            clothes,
-            attributesByClothesId.getOrDefault(clothes.getId(), Collections.emptyList())))
-        .toList();
-
-// 6. OotdDto 리스트 만들기
-    List<OotdDto> ootdDtoList = clothesDtoList.stream()
-        .map(OotdDto::toDto)
-        .toList();
-
-    ///날씨 추천 로직
-    /**
+    /** 날씨 추천 로직
      * 1. 날씨 데이터 가져옴
      * 2. gen-ai에게 프롬포팅해서 결과값(의상 속성중 날씨 관련)을 수치화
      * 3. 해당 수치를 토대로 의상리스트에서, 속성에서 값이 높은것들 산출
@@ -111,6 +64,18 @@ public class RecommendationService {
      * TODO: 1)프로필 위치로, 배치 작업으로 DB저장해 꺼내써 빠른 사용자 속도 경험 제공,
      *       2)새로운 위치시 즉시호출
      */
+
+    //현재 로그인한 유저 정보를 얻는다.
+    UUID userId = authService.getCurrentUserId();
+    User user = userRepository.findById(userId).orElseThrow(
+        () -> new UserNotFoundException(userId)
+    );
+
+    //추천에 필요한 데이터 가져오기
+    double[] weatherData = getWeatherData(weatherId, userId);
+
+    //의상 추천에 필요한 가중치 계산
+    int[] recoWeight = getWeightByAi(weatherData);
 
     //gen-ai 클라이언트
     Client client = new Client();
@@ -134,15 +99,83 @@ public class RecommendationService {
     //todo: 추천의상 저장을 위한 테이블이 필요함.
     List<OotdDto> result = new ArrayList<>();
 
-    return new RecommendationDto(weatherId, userId, ootdDtoList);
+    //todo: 현재 임시 ootd반환.
+
+    return new RecommendationDto(weatherId, userId, getFakeClothes(user));
   }
 
-  private List<Integer> weightByAi(List<Integer> weatherThings, String skyStatus) {
+  //todo: 삭제
+  private List<OotdDto> getFakeClothes(User user) {
+    //페이크 데이터
+    // 1. 의상 리스트 가져오기
+    List<Clothes> clothesList = clothesRepository.findAllByOwner(user);
 
+    // 2. 의상 ID 리스트 추출
+    List<UUID> clothesIds = clothesList.stream()
+        .map(Clothes::getId)
+        .toList();
+
+    // 3. 의상 속성들 한꺼번에 가져오기 (의상 리스트로)
+    List<ClothesAttribute> clothesAttributes = clothesAttributeRepository.findByClothesIn(
+        clothesList);
+
+    // 4. 의상 ID별 속성 매핑 (Map<ClothesId, List<ClothesAttribute>>)
+    Map<UUID, List<ClothesAttribute>> attributesByClothesId = clothesAttributes.stream()
+        .collect(Collectors.groupingBy(attr -> attr.getClothes().getId()));
+
+    // 5. ClothesDto 리스트 만들기
+    List<ClothesDto> clothesDtoList = clothesList.stream()
+        .map(clothes -> clothesUtils.makeClothesDto(
+            clothes,
+            attributesByClothesId.getOrDefault(clothes.getId(), Collections.emptyList())))
+        .toList();
+
+    // 6. OotdDto 리스트 만들기
+    return clothesDtoList.stream()
+        .map(OotdDto::toDto)
+        .toList();
+
+  }
+
+  /**
+   * 추천 의상 산출을 위한 유저와 날씨에 대한 정보를 반환합니다.
+   *
+   * @param weatherId
+   * @return [달(월), 하늘상태, 온도, 습도, 풍속, 프로필 온도 민감도] double 배열
+   */
+  private double[] getWeatherData(UUID weatherId, UUID userId) {
+    Weather weather = weatherRepository.findById(weatherId)
+        .orElseThrow(() -> new WeatherNotFoundException());
+
+    Profile profile = profileRepository.findById(userId)
+        .orElseThrow(() -> new ProfileNotFoundException(userId));
+
+    // 날씨 데이터에서 필요한 데이터를 배열로 저장
+    double[] weatherData = new double[6];
+    weatherData[0] = Double.parseDouble(weather.getForecastAt().toString().substring(5, 7)); //달(월)
+    weatherData[1] = weather.getSkyStatus().getCode(); //하늘상태(구름정도)
+    weatherData[2] = weather.getTemperature().getCurrent(); //현재 온도
+    weatherData[3] = weather.getHumidity(); //습도
+    weatherData[4] = weather.getWind().getSpeed(); //풍속
+    weatherData[5] = profile.getTemperatureSensitivity(); //유저 프로필의 온도 민감도
+
+    return weatherData;
+  }
+
+  /**
+   * LLM api 를 통해 의상 추천 가중치를 계산합니다.
+   *
+   * @param weatherData
+   * @return [계절, 두께감, 안감, 따뜻한 정도]
+   */
+  private int[] getWeightByAi(double[] weatherData) {
+    //     * 의상 속성
+//     * 1. 두께감, 계절,안감 ,따뜻한 정도
+//     * */
     //todo: 배열? 길이가 고정이기 때문에.
-    List<Integer> values = new ArrayList<>();
+    int[] weight = new int[4];
 
-    return values;
+    return weight;
   }
 
 
