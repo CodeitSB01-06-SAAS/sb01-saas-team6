4dc55d218b7a9430a8ab93d6f32e28585151bfd9
kbh <xjvm7001@naver.com>
2025-07-10 17:23:17 +0900

    :recycle: 메서드 파리미터 String 에서 enum 타입으로 변경 #51

--- src/main/java/com/codeit/sb01otbooteam06/domain/feed/service/FeedService.java
+++ src/main/java/com/codeit/sb01otbooteam06/domain/feed/service/FeedService.java
@@ -4,6 +4,8 @@ import com.codeit.sb01otbooteam06.domain.feed.dto.response.FeedDto;
 import com.codeit.sb01otbooteam06.domain.feed.dto.request.FeedCreateRequest;
 import com.codeit.sb01otbooteam06.domain.feed.dto.request.FeedUpdateRequest;
 import com.codeit.sb01otbooteam06.domain.feed.dto.response.FeedDtoCursorResponse;
+import com.codeit.sb01otbooteam06.domain.weather.entity.PrecipitationType;
+import com.codeit.sb01otbooteam06.domain.weather.entity.SkyStatus;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -19,7 +21,7 @@ public interface FeedService {
 
   void deleteFeed(UUID feedId);
 
-  FeedDtoCursorResponse getFeedsByCursor(String keyword, String skyStatus, String precipitationType,
+  FeedDtoCursorResponse getFeedsByCursor(String keyword, SkyStatus skyStatus, PrecipitationType precipitationType,
       Instant cursorCreatedAt, UUID cursorId, Long cursorLikeCount, int size, String sortBy);
 
 }
