1328c92a507b63aecc125f5a0944bcbf21febfab
GiMin0123 <minby260306@gmail.com>
2025-07-11 13:41:59 +0900

    :recycle: 정적 페이지 1.1.3버전으로 수정 #54

--- src/main/java/com/codeit/sb01otbooteam06/domain/user/dto/UserSummary.java
+++ src/main/java/com/codeit/sb01otbooteam06/domain/user/dto/UserSummary.java
@@ -1,5 +1,6 @@
 package com.codeit.sb01otbooteam06.domain.user.dto;
 
+import com.codeit.sb01otbooteam06.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +13,12 @@ public class UserSummary {
     private UUID userId;
     private String name;
     private String profileImageUrl;
+
+    public static UserSummary from(User user) {
+        return UserSummary.builder()
+            .userId(user.getId())
+            .name(user.getName())
+            .profileImageUrl(user.getProfile().getProfileImageUrl())
+            .build();
+    }
 }
